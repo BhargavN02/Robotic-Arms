d0a65adbcbeef228d54e416caf6f505409c16745
Bhargav N <bhargavnama2002@gmail.com>
2022-11-27 00:34:12 +0530

    3R arm dh parameters

--- Robotic arm simulations.py
+++ Robotic arm simulations.py
@@ -3,3 +3,22 @@ import matplotlib.pyplot as plt
 
 # robotic arms with different dh parameters
 
+# 1. 3R arm
+# dh parameters
+d1 = 0.5
+d2 = 0.5
+d3 = 0.5
+a1 = 0.5
+a2 = 0.5
+a3 = 0.5
+alpha1 = 0
+alpha2 = 0
+alpha3 = 0
+
+# print the dh table
+print('dh table for 3R arm')
+print('i', 'a', 'alpha', 'd', 'theta')
+print('1', a1, alpha1, d1, 'theta1')
+print('2', a2, alpha2, d2, 'theta2')
+print('3', a3, alpha3, d3, 'theta3')
+
